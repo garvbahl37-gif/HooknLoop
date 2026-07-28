834a9b885f23b148e4daaa8b4c3dd0fc2179cb45
garvbahl37-gif <garvbahl37@gmail.com>
2026-07-28 13:50:25 +0530

    Remove Australian warehouse line from footer

--- storefront/src/sections/Footer.jsx
+++ storefront/src/sections/Footer.jsx
@@ -35,7 +35,6 @@ export default function Footer() {
           <p className="ft__tag">Industrial hook &amp; loop, dispatched fast across Australia.</p>
           <a className="ft__phone" href="tel:1300183481"><b>1300 183 481</b><span>Call our Australian team</span></a>
           <a className="ft__email" href="mailto:info@hooknloop.com.au">info@hooknloop.com.au</a>
-          <p className="ft__addr">Australian warehouse · Australia-wide dispatch</p>
           <p className="ft__abn">ABN 93 878 995 217</p>
         </div>
 
